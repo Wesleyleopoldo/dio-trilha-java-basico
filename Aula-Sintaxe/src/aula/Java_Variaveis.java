859a4add8c41f63346f795df168bb2166982f615
wesleyestudos <wesley.cauan01@gmail.com>
2024-05-02 13:06:33 -0300

    Adicionando os Primeiros Exercicios

---
+++ Aula-Sintaxe/src/aula/Java_Variaveis.java
@@ -0,0 +1,5 @@
+package aula;
+
+public class Java_Variaveis {
+
+}
---
+++ Aula-Sintaxe/src/aula/aula.java
@@ -0,0 +1,11 @@
+package aula;
+
+public class aula {
+	public static void main(String[] args) {
+		System.out.println("Que blablabla Chato");
+		System.out.println("Socorro Deus");
+		System.out.println("JAVA é muito massa de aprender a codar");
+		System.out.println("Só que os professores complicam demais com várias teorias chatas");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHH!!!!!!!!!!");
+	}
+}
