e6c8c79329e31855c03855bd237081b37a4752dc
Wesley Cauan <wesley.cauan01@gmail.com>
2024-05-03 18:44:13 -0300

    Novo Arquivo Java

---
+++ Aula-Sintaxe/src/aula/app.java
@@ -0,0 +1,17 @@
+public class app {
+    public static void main (String [] args) {
+        // Estude tipos primitivos...
+        // Estude a classe String que representa o texto na aplicação...
+
+        // variaveis
+        double salarioMinimo = 2500;
+        
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+        // constantes
+        final int NUMERO = 5;
+
+        final double VALOR_DE_PI = 3.14;
+    }
+}
